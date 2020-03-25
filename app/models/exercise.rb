ac19aff940a1c5fbc812d5948357ad02a939df7f
Dennis Cope <dennisaaroncope@gmail.com>
2020-03-24 21:45:44 -0700

    Add basic validations and associations for Exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,34 @@
 class Exercise < ApplicationRecord
+        # === Associations ====
+        has_many :muscle_groups, through: :exercise_muscle_groups
+
+        # === Validations ===
+        validates :image_id,                presence: true,
+                                            length: { maximum: 4 },
+                                            numericality: { only_integer: true }
+        validates :name,                    presence: true,
+                                            length: { maximum: 255 },
+                                            uniqueness: true
+        validates :title,                   presence: true,
+                                            length: { maximum: 255 }
+        validates :primer,                  presence: true,
+                                            length: { maximum: 500 }
+        validates :type,                    presence: true,
+                                            length: { maximum: 255 }
+        validates :equipment,               presence: true,
+                                            length: { maximum: 255 }
+        validates :secondary_muscle_groups, presence: true,
+                                            length: { maximum: 255 }
+        validates :steps,                   presence: true,
+                                            length: { maximum: 10_000}
+        validates :tips,                    presence: true,
+                                            length: { maximum: 500 }
+
+        # === Scopes ===
+
+        # === Instance methods ===
+
+        # === Class methods ===
+
+
 end
