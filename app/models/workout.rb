04480040afda1c720257dfe383750e96c073a660
Dennis Cope <dennisaaroncope@gmail.com>
2020-03-25 22:22:56 -0700

    Add basic validations and associations for Workout model

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,13 @@
 class Workout < ApplicationRecord
+    # === Associations ===
+    belongs_to :user
+    has_many :exercises, through: :workout_exercises
+
+    # === Validations ===
+    validates :note,    length: { maximum: 10_000 }
+
+    validates :date,    presence: true
+
+    validates :user_id, presence: true
+                        numericality: { only_integer: true }
 end
