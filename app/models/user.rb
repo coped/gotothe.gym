127d54facb6497196e1713b2441ab0e44cf3d4fe
Dennis Cope <dennisaaroncope@gmail.com>
2020-03-25 22:40:16 -0700

    Add workouts association to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,25 @@
 class User < ApplicationRecord
+    # === Lifecycle callbacks ===
     before_save :downcase_email
-    validates :email, presence: true,
-                      length: { maximum: 255 },
-                      uniqueness: { case_sensitive: false },
-                      format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
-    validates :name, presence: true,
-                     length: { maximum: 255 }
+
+    # === Associations ===
+    has_many :workouts
+
+    # === Validations ===
+    validates :email,    presence: true,
+                         length: { maximum: 255 },
+                         uniqueness: { case_sensitive: false },
+                         format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+
+    validates :name,     presence: true,
+                         length: { maximum: 255 }
+
     has_secure_password
+
     validates :password, length: { minimum: 6 },
                          allow_nil: true
 
+    # === Class methods ===
     def User.digest(string)
         BCrypt::Password.create(string)
     end
