3a8c71d315652c16be6ab44b36ded2baf2f5c870
Dennis Cope <dennisaaroncope@gmail.com>
2020-03-26 19:31:02 -0700

    Add basic tests for MuscleGroup model

--- test/models/muscle_group_test.rb
+++ test/models/muscle_group_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class MuscleGroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @muscle_group = muscle_groups(:example_muscle_group)
+  end
+
+  test "muscle group is valid" do
+    assert @muscle_group.valid?
+  end
+
+  test "muscle group name should always be present" do
+    @muscle_group.name = "     "
+    assert_not @muscle_group.valid?
+  end
+
+  test "muscle group length should not exceed maxumim string length of 255" do
+    @muscle_group.name = "e" * 256
+    assert_not @muscle_group.valid?
+  end
 end
