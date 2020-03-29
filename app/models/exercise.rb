9893369aef4ca8cd68177c302d2d72bc48de28d0
Dennis Cope <dennisaaroncope@gmail.com>
2020-03-29 13:07:59 -0700

    Seed exercise data to database

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -30,4 +30,18 @@ class Exercise < ApplicationRecord
                                         length: { maximum: 10_000 }
                                         
 	validates :tips,                    length: { maximum: 500 }
+
+	# === Class methods ===
+
+	# Parse exercise data from lib/exercises
+	def self.parsed_exercises(exercises = Rails.root.join('lib', 'exercises'))
+		return Dir.children(exercises).reduce([]) do |total, exercise_dir|
+			exercise_files = Dir.children(exercises + exercise_dir)
+			json = exercise_files.find { |file| file.include?(".json") }
+			exercise = JSON.parse(
+				File.read(exercises + exercise_dir + json)
+			)
+			total << exercise
+		end
+	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,49 @@
+# Create mock user data in database
 User.create(
-    {
-        name: "First User",
-        email: "first@user.com",
-        password: "foobar",
-        password_confirmation: "foobar"
-    }
+    name: "First User",
+    email: "first@user.com",
+    password: "foobar",
+    password_confirmation: "foobar"
 )
 
 5.times do 
     User.create(
-        {
-            name: Faker::Name.name,
-            email: Faker::Internet.email,
-            password: "foobar",
-            password_confirmation: "foobar"
-        }
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        password: "foobar",
+        password_confirmation: "foobar"
     )
+end
+
+# Seed exercise data from lib/exercises
+exercises = Exercise.parsed_exercises
+
+muscle_groups = exercises.reduce([]) do |total, exercise|
+    exercise["primary"].each do |group|
+        total << group if !total.include?(group)
+    end
+    total
+end
+
+muscle_groups.each do |muscle_group|
+    MuscleGroup.create(
+        name: muscle_group
+    )
+end
+
+exercises.each do |exercise|
+    new_exercise = Exercise.create(
+        image_id: exercise["id"],
+        name: exercise["name"],
+        title: exercise["title"],
+        primer: exercise["primer"],
+        movement_type: exercise["type"],
+        equipment: exercise["equipment"].join("\n"),
+        secondary_muscle_groups: exercise["secondary"].join("\n"),
+        steps: exercise["steps"].join("\n"),
+        tips: exercise["tips"].join("\n")
+    )
+    exercise["primary"].each do |muscle_group|
+        new_exercise.muscle_groups << MuscleGroup.find_by(name: muscle_group)
+    end
 end
